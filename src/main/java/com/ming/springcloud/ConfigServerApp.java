ea6848c398d25eab9d7c729bf2725da63d8ebca6
chiminwon <chiminwon@gmail.com>
2017-08-10 17:12:07 +0800

    Initial

---
+++ src/main/java/com/ming/springcloud/ConfigServerApp.java
@@ -0,0 +1,15 @@
+package com.ming.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApp {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConfigServerApp.class).web(true).run(args);
+	}
+
+}
