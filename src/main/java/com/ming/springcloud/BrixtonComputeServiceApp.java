a1d197a75f8d71fa59da1bf9cc1ab50f9469f6f8
chiminwon <chiminwon@gmail.com>
2017-08-10 17:05:55 +0800

    Initial commit

---
+++ src/main/java/com/ming/springcloud/BrixtonComputeServiceApp.java
@@ -0,0 +1,15 @@
+package com.ming.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BrixtonComputeServiceApp {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(BrixtonComputeServiceApp.class).web(true).run(args);
+	}
+
+}
