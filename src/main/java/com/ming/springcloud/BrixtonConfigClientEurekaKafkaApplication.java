091c11724c589492aad6876855380c626bb516c4
chiminwon <chiminwon@gmail.com>
2017-08-28 23:58:23 +0800

    Initial commit

---
+++ src/main/java/com/ming/springcloud/BrixtonConfigClientEurekaKafkaApplication.java
@@ -0,0 +1,14 @@
+package com.ming.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BrixtonConfigClientEurekaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrixtonConfigClientEurekaKafkaApplication.class, args);
+	}
+}
